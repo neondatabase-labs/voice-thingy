7ba8cd3c858dd524f5eabb2ae4aa93c7bfee737e
Rishi Raj Jain <rishi@neon.tech>
2024-11-19 02:07:23 +0530

    wrap

--- app/components/Message.tsx
+++ app/components/Message.tsx
@@ -3,7 +3,7 @@ import { Cpu, User } from 'react-feather'
 
 export default function ({ conversationItem }: { conversationItem: ItemType }) {
   return (
-    <div className="flex flex-row items-start gap-x-3">
+    <div className="flex flex-row items-start gap-x-3 flex-wrap max-w-full">
       <div className="rounded border p-2 max-w-max">{conversationItem.role === 'user' ? <User /> : <Cpu />}</div>
       <div className="flex flex-col gap-y-2">
         {conversationItem.role === 'user' && <div>{conversationItem.formatted.transcript || (conversationItem.formatted.audio?.length ? '(awaiting transcript)' : conversationItem.formatted.text || '(item sent)')}</div>}
