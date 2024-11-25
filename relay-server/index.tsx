bff492216e420ba563aa205fcfc207aee0185342
Rishi Raj Jain <rishi@neon.tech>
2024-11-25 14:22:25 +0530

    change

--- relay-server/index.tsx
+++
@@ -1,9 +0,0 @@
-import 'dotenv/config'
-import RealtimeRelay from './lib/relay'
-
-const OPENAI_API_KEY = process.env.OPENAI_API_KEY
-if (!OPENAI_API_KEY) throw new Error('Environment variable OPENAI_API_KEY is required. Please set it in your .env file.')
-
-const PORT = process.env.PORT ? parseInt(process.env.PORT) : 8081
-
-new RealtimeRelay(OPENAI_API_KEY).listen(PORT)
